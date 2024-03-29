e256042b523bc8c74394d3f55d3f905f6bf0e248
Denis Frolkov <deniska-frolkov@mail.ru>
2024-03-30 00:16:26 +0300

    Initial commit

---
+++ app/src/main/java/com/example/ntiteam_test_work_foodies/MainActivity.kt
@@ -0,0 +1,47 @@
+package com.example.ntiteam_test_work_foodies
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.example.ntiteam_test_work_foodies.ui.theme.Ntiteam_test_work_FoodiesTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            Ntiteam_test_work_FoodiesTheme {
+                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                    Greeting(
+                        name = "Android",
+                        modifier = Modifier.padding(innerPadding)
+                    )
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Text(
+        text = "Hello $name!",
+        modifier = modifier
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    Ntiteam_test_work_FoodiesTheme {
+        Greeting("Android")
+    }
+}
---
+++ app/src/main/java/com/example/ntiteam_test_work_foodies/sreens/CatalogScreen.kt
@@ -0,0 +1,9 @@
+package com.example.ntiteam_test_work_foodies.sreens
+
+fun CatalogScreen() {
+    Catalog()
+}
+
+fun Catalog(){
+
+}
